21c2a054d39e13888ebc951036ec8028f430a9c4
LoshiniWelonika <welonikakedapanawala@gmail.com>
2026-01-16 16:04:48 +0530

    HomePage UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,22 +4,14 @@ import './index.css';
 import HomePage from './pages/HomePage';  
 import CreatePage from './pages/CreatePage';  
 import NoteDetails from './pages/NoteDetails'; 
-import toast, { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <div data-theme="forest">
        {/* Toast container */}
       <Toaster position="top-right" reverseOrder={false} />
-
-      <button onClick={()=>toast.success("Congrats")} className='btn btn-primary'>Click Me</button>
-      <button class="btn btn-active">Default</button>
-      <button class="btn btn-active btn-neutral">Neutral</button>
-      <button class="btn btn-active btn-primary">Primary</button>
-      <button class="btn btn-active btn-secondary">Secondary</button>
-      <button class="btn btn-active btn-accent">Accent</button>
-      <button class="btn btn-active btn-ghost">Ghost</button>
-      <button class="btn btn-active btn-link">Link</button>
+      
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/create" element={<CreatePage />}></Route>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import NavBar from "../components/NavBar";
+import { useState } from 'react';
+import RateLimitedUI from '../components/RateLimitedUI';
+import axios from 'axios';
 
 const HomePage = () => {
+  const [IsRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect( () => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.post("https://localhost:5000/api/notes");
+        console.log(res.data);
+      } catch (error) {
+        console.error("Error Fetching notes", error)
+      }
+    }
+    fetchNotes();
+  }, [])
+
   return (
-    <div>HomePage</div>
+    <div className='min-h-screen'>
+      <NavBar />
+
+      {IsRateLimited && <RateLimitedUI/>}
+    </div>
   )
 }
 
